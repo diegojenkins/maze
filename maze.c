363e60b536dc34d91bc02c7dedca3cd168aed7a6
diegojenkins <diegothejenkins@gmail.com>
2016-04-11 20:55:34 -0700

    Update maze.c

--- maze.c
+++ maze.c
@@ -1,272 +1,224 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 typedef struct {
-    int x, y;
-} Point;
+    int rows, columns;
+} Matrix;
+
+bool read_input(char * file_dest, Matrix * size, Matrix * entry, Matrix * exit, char *** maze);
+void find_path(char ** maze, Matrix size, Matrix entry, Matrix exit);
+void display_maze(char ** maze, Matrix size, Matrix curr);
+void go_forward(Matrix * curr, int dir);
+int turn_right(int dir);
+int turn_left(int dir);
+bool is_wall_next_to(char ** maze, Matrix size, Matrix * pos, int dir);
+bool is_wall(char ** maze, Matrix size, Matrix pos);
+bool is_in_maze(Matrix size, Matrix pos);
 
 
-int turnLeft(int dir);
-int turnRight(int dir);
 
-
-void go_forward(Point * curr, int dir);
-
-char ** read_input(char filename[], Point * maze_size, Point * start, Point * end, char ** maze);
-char ** create_2d_array(int number_rows, int number_columns, char ** arr);
-
-void find_path(char ** maze, Point * maze_size, Point * start, Point * end);
-
+/* client */
 int main(int argc, char ** argv) {
-    int i, j;
-    char * fname = "maze2.txt";
+    char ** maze;
+    Matrix size;
+    Matrix entry;
+    Matrix exit;
+    int i;
     
-    printf("Hey!\n");
+	/* If no filename was entered, abort the program. */
+    if (argc < 2) {
+        printf("No maze name was entered. \n");
+        return 1;
+    }
+
+	
+    if (!read_input(argv[1], &size, &entry, &exit, &maze)) {
+        printf("The file you tried to open, \"%s\" doesn't exist.\n", argv[1]);
+        return 1;
+    }
+   
+
+        printf("The unsolved maze: \n");
+        printf("The starting position is (%d,%d) and the end is (%d,%d), from the upper left.\n", entry.rows,  entry.columns,  exit.rows, exit.columns);
+    display_maze(maze, size, exit); /* display the maze before it is solved*/
+ 
+    find_path(maze, size, entry, exit); /* solve the maze */
+       
+	 printf("The solved maze:\n");
+        printf("The starting position is (%d,%d) and the end is (%d,%d), from the upper left.\n", entry.rows,  entry.columns,  exit.rows, exit.columns);
+    display_maze(maze, size, exit); /* display the maze after it is solved*/
     
-    Point maze_size;
-    Point start;
-    Point end;
+    /* deallocate the memory of the maze */
+    for (i = 0; i < size.rows; i++)
+        free(maze[i]);
+    free(maze);
+    return 0;
+
+}
+
+/*
+Coordinates are stored with the convention that maze[x][y] accesses the xth column from the left,
+and the yth row from the top of the maze.
+*/
+
+/* read in the maze file and extract all information */
+bool read_input(char * file_dest, Matrix * size, Matrix * entry, Matrix * exit, char *** maze) {
+    FILE * file;
+    char * next_line;
+    int i, j, SIZE = 500;
     
-    //char ** maze;
-    
-    //Don;t think about his picutre.
-    //First index is the horizontal axis, accessed by x positions within the code.
-    //The second index is the vertical axis, accessed by y within the code.
-    
-    char maze[4][16] ={
-        {'X', 'X', 'X', 'X', 'X', '0', 'X', 'X', '0', '0', '0', '0', '0', '0', '0', '0'},
-        {'X', 'X', 'X', 'X', 'X', '0', 'X', 'X', '0', 'X', 'X', 'X', '0', '0', '0', '0'},
-        {'0', '0', 'X', 'X', 'X', '0', 'X', 'X', '0', 'X', 'X', 'X', '0', '0', '0', '0'},
-        {'0', '0', 'X', 'X', 'X', '0', '0', '0', '0', '0', 'X', '0', '0', '0', '0', '0'}
-    };
-    
-    maze_size.x = 4;
-    maze_size.y = 16;
-    
-    start.x = 0;
-    start.y = 5;
-    
-    end.x = 2;
-    end.y = 15;
-    
-    /*
-     printf("argc: %d\n", argc);
-     for (i = 0; i < argc; ++i) {
-     printf("argv[%d]: %s \n", i, argv[i]);
-     }*/
-    
-    //maze = read_input(fname, &maze_size, &start, &end, maze);
-    /*
-     maze = create_2d_array(width, height, maze);
-     
-     for (i = 0; i < width; ++i)
-     {
-     for (j = 0; j < height; ++j)
-     {
-     printf("i: %d j: %d\n", i, j);
-     maze[i][j] = 63 + i+j;
-     }
-     printf("\n");
-     }
-     */
-    for (j = 0; j < maze_size.y; ++j) {
-        for (i = 0; i < maze_size.x; ++i) {
+    if ((file = fopen(file_dest, "r")) == NULL)
+        return false;
+    else {
+        next_line = malloc(sizeof (char) * SIZE);
+        fgets(next_line, SIZE, file); /* get maze's size */
+        sscanf(next_line, "%d,%d", &(size->columns), &(size->rows));
+        fgets(next_line, SIZE, file); /* get maze's entry */
+        sscanf(next_line, "%d,%d", &(entry->columns), &(entry->rows));
+        fgets(next_line, SIZE, file); /* get maze's exit */
+        sscanf(next_line, "%d,%d", &(exit->columns), &(exit->rows));
         
-            
-            printf("%c", maze[i][j]);
+        /* allocate the memory for the given maze */
+        *maze = malloc(sizeof (char *) * size->rows);
+        for (i = 0; i < size->rows; ++i)
+            (*maze)[i] = malloc(sizeof (char) * size->columns);
+        
+        /* copy the maze from the file to the 2d-array */
+        for (i = 0; i < size->rows; ++i) {
+            for (j = 0; j < size->columns; ++j)
+                (*maze)[i][j] = fgetc(file);
+            fgetc(file);
         }
+        fclose(file);
+    }
+
+	free(next_line);
+    return true;
+}
+
+
+/* */
+void find_path(char ** maze, Matrix size, Matrix entry, Matrix exit) {
+    Matrix current; /*The current position of the walker within the maze.*/
+	/*
+	Directions are stored as an int, with the convention 0 = north, 1 = east, 2 = south, 3 = west.
+	*/
+
+    int dir = 2; /* South */
+    
+    current.rows = entry.rows;
+    current.columns = entry.columns;
+    
+    while (!((current.rows == exit.rows) && (current.columns == exit.columns))) {
+        maze[current.rows][current.columns] = 'W';
+        if (!is_wall_next_to(maze, size, &current, turn_right(dir))) { /* no wall to the right */
+            dir = turn_right(dir);
+            go_forward(&current, dir);
+        } else if (!is_wall_next_to(maze, size, &current, dir)) { /* no wall ahead */
+            go_forward(&current, dir);
+        } else if (!is_wall_next_to(maze, size, &current, turn_left(dir))) { /* no wall to the left */
+            dir = turn_left(dir);
+            go_forward(&current, dir);
+        } else { /* no wall behind, turn 180 */
+            dir = turn_left(dir);
+            dir = turn_left(dir);
+            go_forward(&current, dir);
+        }
+    }
+    maze[current.rows][current.columns] = 'W';
+}
+
+
+
+/* Display the maze */
+void display_maze(char ** maze, Matrix size, Matrix curr) {
+    int i, j;
+    for (i = 0; i < size.rows; ++i) {
+        for (j = 0; j < size.columns; ++j)
+            printf("%c", maze[i][j]);
         printf("\n");
     }
-    
-    
-    
-    return 0;
 }
 
-int turnLeft(int dir) {
-    return (dir - 1 + 4) % 4;
+
+
+
+/* */
+void go_forward(Matrix * curr, int dir) {
+    switch (dir) {
+        case 0: /* north */
+            --curr->rows;
+            break;
+        case 1: /* east */
+            ++curr->columns;
+            break;
+        case 2: /* south */
+            ++curr->rows;
+            break;
+        case 3: /* west */
+            --curr->columns;
+            break;
+        default:
+            break;
+    }
 }
 
-int turnRight(int dir) {
+
+
+
+/* */
+int turn_right(int dir) {
     return (dir + 1) % 4;
 }
 
-void go_forward(Point * curr, int dir)
-{
-    switch (dir)
-    {
-        case 0: //north
-            --curr->x;
-            break;
-        case 1: //east
-            ++curr->y;
-            break;
-        case 2: //south
-            ++curr->x;
-            break;
-        case 3: //west
-            --curr->y;
-            break;
-        default:
-            printf("you are eaten by the minotaur");
-            break;
-    }
+
+
+
+/* */
+int turn_left(int dir) {
+    return (dir - 1 + 4) % 4;
 }
 
-bool is_in_maze(Point maze_size, Point pos)
-{
-    if (pos.x < 0)
+
+
+
+/* */
+bool is_wall_next_to(char ** maze, Matrix size, Matrix * pos, int dir) {
+    Matrix target;
+    target.rows = pos->rows;
+    target.columns = pos->columns;
+    
+    go_forward(&target, dir);
+    return is_wall(maze, size, target);
+    
+}
+
+
+
+
+/* */
+bool is_wall(char ** maze, Matrix size, Matrix pos) {
+    if (!is_in_maze(size, pos))
+        return true;
+    return (maze[pos.rows][pos.columns] == 'X');
+}
+
+
+
+
+/* */
+bool is_in_maze(Matrix size, Matrix pos) {
+    if (pos.rows < 0)
         return false;
-    if (pos.x >= maze_size.x)
+    if (pos.rows >= size.rows)
         return false;
-    if (pos.y < 0)
+    if (pos.columns < 0)
         return false;
-    if (pos.y >= maze_size.y)
+    if (pos.columns >= size.columns)
         return false;
     return true;
 }
 
-bool is_wall(char ** maze, Point maze_size, Point pos)
-{
-    if (!is_in_maze(maze_size, pos))
-        return true;
-    return (maze[pos.x][pos.y] == 'X');
-}
 
-//bool is_wall_left()
-bool is_wall_next_to(char ** maze, Point maze_size, Point * pos, int dir)
-{
-    Point target;
-    target.x = pos->x;
-    target.y = pos->y;
-    
-    go_forward(&target, dir);
-    return is_wall(maze, maze_size, target);
-    
-}
-
-
-void find_path(char ** maze, Point * maze_size, Point * start, Point * end)
-{
-    int dir = 2; //South
-    bool solved = false;
-    Point curr;
-    
-    curr.x = start->x;
-    curr.y = start->y;
-    
-    while (!solved)
-    {
-        if (!is_wall_next_to(maze, &maze_size, &curr, dir)) //no wall ahead
-        {
-            go_forward(&curr, dir);
-        }
-        else if (!is_wall_next_to(maze, &maze_size, &curr, turn_right(dir))) // no wall to the right
-        {
-            dir = turnRight(dir);
-            go_forward(&curr, dir);
-        }
-        else if (!is_wall_next_to(maze, &curr, turn_left(dir))) // no wall to the right
-        {
-            dir = turnRight(dir);
-            go_forward(&curr, dir);
-
-        }
-        
-        if ((curr.x == end->x) &&(curr.y == end->y) )
-            solved = true;
-    }
-    
-}
-
-
-//
-//char ** create_2d_array(int nrows, int ncolumns, char ** array) {
-//    int i;
-//
-//    printf("rows: %d, columns: %d\n", nrows, ncolumns);
-//    array = malloc(nrows * sizeof (char *));
-//    for (i = 0; i < nrows; i++) {
-//        printf("current row: %d\n", i);
-//        array[i] = malloc(ncolumns * sizeof (char));
-//    }
-//    /*
-//     for (i = 0; i < nrows; ++i)
-//     {
-//     for (j = 0; j < ncolumns; ++j)
-//     {
-//     printf("place to add: i: %d j: %d\n", i, j);
-//     array[i][j] = 63 + i+j;
-//     printf("value at i: %d j: %d is %d\n", i, j, array[i][j]);
-//     }
-//     printf("\n");
-//     }
-//
-//     */
-//
-//    /*
-//     int i;
-//     arr = malloc( number_rows *sizeof(char*));
-//     for (i = 0; i < number_rows; i++)
-//     {
-//     arr[i] = malloc(number_columns *sizeof(char));
-//     }*/
-//    return array;
-//}
-
-char ** read_input(char filename[], Point * maze_size, Point * start, Point * end, char ** maze) {
-    FILE *fp;
-    int SIZE = 6000;
-    char line[SIZE];
-    int line_number = 0;
-    
-    int current_row, current_column;
-    /*
-     int width;
-     int height;
-     
-     int startx, starty;
-     int endx, endy;
-     */
-    if ((fp = fopen(filename, "r")) == NULL) {
-        printf("File open not successful.\n");
-        return 0;
-    } else {
-        while (fgets(line, SIZE, fp) != NULL) {
-            if (line_number == 0) {
-                sscanf(line, "%d,%d", &(maze_size->x), &(maze_size->y));
-                printf("width: %d, height: %d\n", maze_size->x, maze_size->y);
-            } else if (line_number == 1) {
-                sscanf(line, "%d,%d", &(start->x), &(start->y));
-                //printf("startx: %d, starty: %d\n", startx, starty);
-            } else if (line_number == 2) {
-                sscanf(line, "%d,%d", &(end->x), &(end->y));
-                //printf("end x: %d, end y: %d\n", endx, endy);
-            } else if (line_number == 3) {
-                current_row = 0;
-                //maze = create_2d_array(maze_size->x, maze_size->y, maze);
-            }
-            
-            if (line_number >= 3) {
-                current_column = 0;
-                while (line[current_column] != '\0') {
-                    printf("row: %d col: %d \n", current_row, current_column);
-                    maze[current_row][current_column] = line[current_column];
-                    current_column++;
-                }
-                current_row++;
-            }
-            //sscanf(line, "%lf %lf %lf %lf ", &charges[i].charge, &charges[i].x, &charges[i].y, &charges[i].z);
-            //printf("%s ", line);
-            ++line_number;
-        }
-        
-        printf("File open successful.\n");
-        if (fclose(fp) == EOF) {
-            printf("File close not successful.\n");
-            return 0;
-        }
-    }
-    return maze;
-}
